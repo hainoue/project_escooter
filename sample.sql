8e255b340aefca80c3c9dce932ee8dce38911803
Hajime Inoue <63652353+hainoue@users.noreply.github.com>
2022-08-14 15:38:27 -0400

    Update sample.sql

--- sample.sql
+++ sample.sql
@@ -1,19 +1,23 @@
+-- Script to find number of trips per community per hour
 SELECT 
   StartArea,
   StartTime,
   COUNT(ID) as NumberOfTrips
 FROM
+-- Modified table with needed fields only
   (SELECT 
     TripID as ID,
-    left(StartTime,10) as StartTime,
+    left(StartTime,10) as StartTime,  -- Eliminating 'AM' and 'PM' string because they're not standard format
     TripDistance as TripDist,
-    TripDuration/60 as TripDur,
+    TripDuration/60 as TripDur,       -- Original was in seconds and hard to get the idea
     StartCommunityAreaName as StartArea,
     EndCommunityAreaName as EndArea
   FROM `capstoneproject-357619.escooter_chicago.trips2020`
+  -- not including rows with empty area-name
   WHERE StartCommunityAreaName IS NOT NULL AND EndCommunityAreaName IS NOT NULL
   )
--- WHERE StartArea = EndArea
+-- Grouping with area it started and time it started respectively
 GROUP BY StartArea, StartTime
--- ORDER BY StartArea
+
+-- To preveiw, use the following line
 -- LIMIT 10
